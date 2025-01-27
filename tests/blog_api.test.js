ac8a39544bd8458a4cf705e1ecbfe7c54067f534
Sami Heinimo <sami.heinimo@helsinki.fi>
2025-01-27 11:17:58 +0200

    Part 4 exercise 11

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -52,6 +52,25 @@ test('A valid blog can be added', async () => {
     assert(titles.includes('How to Keep Your People from Falling into Despair'))
 })
 
+test('If likes are not specified, default to 0', async () => {
+    const newBlog = {
+        title: 'Mental Health and Radiance',
+        author: 'Kaladin Stormblessed',
+        url: 'https://urithi.ru/w1ndrunn3r'
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    const likes = response.body.map(r => r.likes)
+
+    assert.strictEqual(likes.at(-1), 0)
+})
+
 after(async () => {
     await mongoose.connection.close()
 })
